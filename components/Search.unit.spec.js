e5da96081d024d1c99ec46e8e758bc77c728896b
Otávio Silva <otaviosilva2632@gmail.com>
2023-06-15 21:07:29 -0300

    feat: preventing submit event in form

--- components/Search.unit.spec.js
+++ components/Search.unit.spec.js
@@ -9,13 +9,26 @@ describe('Search - unit', () => {
 
   it('should emit the search event when form is submitted', async () => {
     const wrapper = mount(Search);
-
     const term = 'term for search';
+
     await wrapper.find('input[type="search"]').setValue(term);
     await wrapper.find('form').trigger('submit');
 
     expect(wrapper.emitted().doSearch).toBeTruthy();
     expect(wrapper.emitted().doSearch.length).toBe(1);
     expect(wrapper.emitted().doSearch[0]).toEqual([{ term }]);
+  });
+
+  it('should emit the search event when search input is cleard', async () => {
+    const wrapper = mount(Search);
+    const term = 'term for search';
+    const input = wrapper.find('input[type="search"]');
+
+    await input.setValue(term);
+    await input.setValue('');
+
+    expect(wrapper.emitted().doSearch).toBeTruthy();
+    expect(wrapper.emitted().doSearch.length).toBe(1);
+    expect(wrapper.emitted().doSearch[0]).toEqual([{ term: '' }]);
   });
 });
