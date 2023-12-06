47be8088a79b831096a363389e1cf57481e18c71
Otávio Silva <otaviosilva2632@gmail.com>
2023-12-06 15:11:36 -0300

    feat: finish testing for products search

--- cypress/e2e/Store.cy.js
+++ cypress/e2e/Store.cy.js
@@ -21,22 +21,31 @@ context('Store', () => {
   context('Store > Search for products', () => {
     it('should type in the search field', () => {
       cy.visit('http://localhost:3000');
-
       cy.get('input[type="search"]')
         .type('some text here')
         .should('have.value', 'some text here');
     });
 
-    it.only('should type in the search field', () => {
+    it('should return one product when "beautiful watch" is used as search term', () => {
       server.create('product', {
         title: 'beautiful watch',
       });
       server.createList('product', 10);
-      cy.visit('http://localhost:3000');
 
+      cy.visit('http://localhost:3000');
       cy.get('input[type="search"]').type('beautiful watch');
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should('have.length', 1);
+    });
+
+    it('should return any product', () => {
+      server.createList('product', 10);
+
+      cy.visit('http://localhost:3000');
+      cy.get('input[type="search"]').type('beautiful watch');
+      cy.get('[data-testid="search-form"]').submit();
+      cy.get('[data-testid="product-card"]').should('have.length', 0);
+      cy.get('body').contains('0 Products');
     });
   });
 });
