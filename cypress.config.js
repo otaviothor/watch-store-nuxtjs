7efde553e4a47a73dd646a479e024aaa45354dfe
Otávio Silva <otaviosilva2632@gmail.com>
2023-12-06 23:19:47 -0300

    feat: testing product list

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
     supportFile: './cypress/support/index.js',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/Store.cy.js
+++ cypress/e2e/Store.cy.js
@@ -12,15 +12,39 @@ context('Store', () => {
   });
 
   it('should display the store', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
 
     cy.get('body').contains('Brand');
     cy.get('body').contains('Wrist Watch');
   });
 
+  context('Store > Product List', () => {
+    it('should display "0 Products" when no products is returned', () => {
+      cy.visit('/');
+      cy.get('[data-testid="product-card"]').should('have.length', 0);
+      cy.get('body').contains('0 Products');
+    });
+
+    it('should display "1 Product" when 1 product is returned', () => {
+      server.create('product');
+
+      cy.visit('/');
+      cy.get('[data-testid="product-card"]').should('have.length', 1);
+      cy.get('body').contains('1 Product');
+    });
+
+    it('should display "10 Products" when 10 products is returned', () => {
+      server.createList('product', 10);
+
+      cy.visit('/');
+      cy.get('[data-testid="product-card"]').should('have.length', 10);
+      cy.get('body').contains('10 Products');
+    });
+  });
+
   context('Store > Search for products', () => {
     it('should type in the search field', () => {
-      cy.visit('http://localhost:3000');
+      cy.visit('/');
       cy.get('input[type="search"]')
         .type('some text here')
         .should('have.value', 'some text here');
@@ -32,7 +56,7 @@ context('Store', () => {
       });
       server.createList('product', 10);
 
-      cy.visit('http://localhost:3000');
+      cy.visit('/');
       cy.get('input[type="search"]').type('beautiful watch');
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should('have.length', 1);
@@ -41,7 +65,7 @@ context('Store', () => {
     it('should return any product', () => {
       server.createList('product', 10);
 
-      cy.visit('http://localhost:3000');
+      cy.visit('/');
       cy.get('input[type="search"]').type('beautiful watch');
       cy.get('[data-testid="search-form"]').submit();
       cy.get('[data-testid="product-card"]').should('have.length', 0);
